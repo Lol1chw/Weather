ef7ed2d53f9eb146be765ea5ea8363809055dd14
Shl <94941930+Lol1chw@users.noreply.github.com>
2023-02-09 14:01:33 +0700

    template for the new functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import Card from "./components/card1";
 function App() {
   return (
     <div className="container">
+      <div className="header">
+        <h1>WeatherWatch</h1>
+        <div className="navMenu">
+          <a>Today</a>
+          <a>Tommorow</a>
+        </div>
+      </div>
       <Card />
     </div>
   );
